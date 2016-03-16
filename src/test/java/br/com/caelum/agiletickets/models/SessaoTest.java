c9999f18f8f322c172ea265e2edd929c84fa55d2
Rafael Conrado <conradorafa@gmail.com>
2016-03-16 19:13:34 -0300

    novo teste vender 2 ingresso se ha 2 vagas - SessaoTest

--- src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
@@ -12,6 +12,13 @@ public class SessaoTest {
 
         Assert.assertTrue(sessao.podeReservar(1));
 	}
+	@Test
+	public void deveVender2IngressoSeHa2vagas() throws Exception {
+		Sessao sessao = new Sessao();
+        sessao.setTotalIngressos(2);
+
+        Assert.assertTrue(sessao.podeReservar(2));
+	}
 	
 	@Test
 	public void deveVender5ingressosSeHa10vagas() throws Exception {
