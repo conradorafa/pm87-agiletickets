e3c71ab5857e257ee482e4a8eab17781def3fba0
Rafael Conrado <conradorafa@gmail.com>
2016-03-16 22:00:37 -0300

    refactoring calculaPreco no Enum TipoDeEspetaculo

--- src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
+++ src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
@@ -11,46 +11,11 @@ public class CalculadoraDePrecos {
 		BigDecimal preco;
 		
 		TipoDeEspetaculo tipoDoEspetaculo = sessao.getEspetaculo().getTipo();
-		if(tipoDoEspetaculo.equals(TipoDeEspetaculo.CINEMA) || tipoDoEspetaculo.equals(TipoDeEspetaculo.SHOW)) {
-			if(calculaPercentagemDeIngressosDisponiveis(sessao) <= 0.05) { 
-				preco = calculaPrecoComAdicional(sessao, 0.10);
-			} else {
-				preco = sessao.getPreco();
-			}
-		} else if(tipoDoEspetaculo.equals(TipoDeEspetaculo.BALLET)) {
-			if(calculaPercentagemDeIngressosDisponiveis(sessao) <= 0.50) { 
-				preco = calculaPrecoComAdicional(sessao, 0.20);
-			} else {
-				preco = sessao.getPreco();
-			}
-			
-			if(sessao.getDuracaoEmMinutos() > 60){
-				preco = preco.add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
-			}
-		} else if(tipoDoEspetaculo.equals(TipoDeEspetaculo.ORQUESTRA)) {
-			if(calculaPercentagemDeIngressosDisponiveis(sessao) <= 0.50) { 
-				preco =  calculaPrecoComAdicional(sessao, 0.20);
-			} else {
-				preco = sessao.getPreco();
-			}
-
-			if(sessao.getDuracaoEmMinutos() > 60){
-				preco = preco.add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
-			}
-		}  else {
-			
-			preco = sessao.getPreco();
-		} 
+		preco = tipoDoEspetaculo.calculaPreco(sessao);
 
 		return preco.multiply(BigDecimal.valueOf(quantidade));
 	}
 
-	private static BigDecimal calculaPrecoComAdicional(Sessao sessao, double percentual) {
-		return sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(percentual)));
-	}
 
-	private static double calculaPercentagemDeIngressosDisponiveis(Sessao sessao) {
-		return (sessao.getTotalIngressos() - sessao.getIngressosReservados()) / sessao.getTotalIngressos().doubleValue();
-	}
 
 }
--- src/main/java/br/com/caelum/agiletickets/models/TipoDeEspetaculo.java
+++ src/main/java/br/com/caelum/agiletickets/models/TipoDeEspetaculo.java
@@ -1,7 +1,84 @@
 package br.com.caelum.agiletickets.models;
 
+import java.math.BigDecimal;
+
 public enum TipoDeEspetaculo {
 	
-	CINEMA, SHOW, TEATRO, BALLET, ORQUESTRA;
+	CINEMA {
+		public BigDecimal calculaPreco(Sessao sessao){
+			BigDecimal preco;
+			if(calculaPercentagemDeIngressosDisponiveis(sessao) <= 0.05) { 
+				preco = calculaPrecoComAdicional(sessao, 0.10);
+			} else {
+				preco = sessao.getPreco();
+			}
+			return preco;
+		}
+	}, 
+	SHOW {
+		@Override
+		public BigDecimal calculaPreco(Sessao sessao) {
+			// TODO Auto-generated method stub
+			BigDecimal preco;
+			if(calculaPercentagemDeIngressosDisponiveis(sessao) <= 0.05) { 
+				preco = calculaPrecoComAdicional(sessao, 0.10);
+			} else {
+				preco = sessao.getPreco();
+			}
+			return preco;
+		}
+	}, 
+	TEATRO {
+		@Override
+		public BigDecimal calculaPreco(Sessao sessao) {
+			// TODO Auto-generated method stub
+			BigDecimal preco;
+			preco = sessao.getPreco();
+			return preco;
+		}
+	}, 
+	BALLET {
+		@Override
+		public BigDecimal calculaPreco(Sessao sessao) {
+			// TODO Auto-generated method stub
+			BigDecimal preco;
+			if(calculaPercentagemDeIngressosDisponiveis(sessao) <= 0.50) { 
+				preco = calculaPrecoComAdicional(sessao, 0.20);
+			} else {
+				preco = sessao.getPreco();
+			}
+			
+			if(sessao.getDuracaoEmMinutos() > 60){
+				preco = preco.add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
+			}
+			return preco;
+		}
+	}, 
+	ORQUESTRA {
+		@Override
+		public BigDecimal calculaPreco(Sessao sessao) {
+			// TODO Auto-generated method stub
+			BigDecimal preco;
+			if(calculaPercentagemDeIngressosDisponiveis(sessao) <= 0.50) { 
+				preco =  calculaPrecoComAdicional(sessao, 0.20);
+			} else {
+				preco = sessao.getPreco();
+			}
+
+			if(sessao.getDuracaoEmMinutos() > 60){
+				preco = preco.add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
+			}
+			return preco;
+		}
+	};
 	
+	public abstract BigDecimal calculaPreco(Sessao sessao);
+	
+	private static BigDecimal calculaPrecoComAdicional(Sessao sessao, double percentual) {
+		return sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(percentual)));
+	}
+
+	private static double calculaPercentagemDeIngressosDisponiveis(Sessao sessao) {
+		return (sessao.getTotalIngressos() - sessao.getIngressosReservados()) / sessao.getTotalIngressos().doubleValue();
+	}
 }
