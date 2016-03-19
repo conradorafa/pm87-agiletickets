792af808e3b2949af39835ca3cf28c83132329c7
Rafael Conrado <conradorafa@gmail.com>
2016-03-18 22:08:01 -0300

    novo teste reserva

---
+++ src/acceptance-test/java/br/com/caelum/agiletickets/acceptance/ReservaTest.java
@@ -0,0 +1,26 @@
+package br.com.caelum.agiletickets.acceptance;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class ReservaTest {
+
+	
+	private static WebDriver browser;
+
+	@Before
+	public void setUp() throws Exception {
+		browser = new FirefoxDriver();
+	}
+
+	@After
+	public static void teardown() {
+		browser.close();
+	}
+
+	
+	
+}
