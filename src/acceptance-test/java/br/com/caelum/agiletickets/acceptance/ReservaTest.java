9385c96438209405b031848d03c8f04147f653e3
Rafael Conrado <conradorafa@gmail.com>
2016-03-18 22:12:37 -0300

    novo teste reserva

--- src/acceptance-test/java/br/com/caelum/agiletickets/acceptance/ReservaTest.java
+++ src/acceptance-test/java/br/com/caelum/agiletickets/acceptance/ReservaTest.java
@@ -2,6 +2,7 @@ package br.com.caelum.agiletickets.acceptance;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -19,6 +20,11 @@ public class ReservaTest {
 	@After
 	public static void teardown() {
 		browser.close();
+	}
+	
+	@Test
+	public void deveAdicionar10porcentoNosUltimos5Ingressos(){
+		
 	}
 
 	
